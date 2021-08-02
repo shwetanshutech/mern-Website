8351c57db93ed47479d37e42418f440c06cff1ed
shwetanshutech <shwetanshu2000@gmail.com>
2021-08-02 13:32:50 +0530

    navbar added

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.css";
-function navBar() {
+
+function Navbar() {
   return (
     <>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -11,19 +11,18 @@ function navBar() {
           class="navbar-toggler"
           type="button"
           data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span class="navbar-toggler-icon"></span>
         </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
               <a class="nav-link" href="#">
-                Home
+                Home <span class="sr-only">(current)</span>
               </a>
             </li>
             <li class="nav-item">
@@ -37,13 +36,13 @@ function navBar() {
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">
+              <a class="nav-link " href="#">
                 Login
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">
-                Register
+              <a class="nav-link " href="#">
+                SignUp
               </a>
             </li>
           </ul>
@@ -53,4 +52,4 @@ function navBar() {
   );
 }
 
-export default navBar;
+export default Navbar;
